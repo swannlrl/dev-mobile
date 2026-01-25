90fa0aa93bb9b905352f26890b70a06a20d11496
Swann Le Roy Liberge <swann02.lrl@gmail.com>
2026-01-25 22:57:20 +0100

    finish widget

--- cours_ece_2026/cours_01/lib/login/login_page.dart
+++ cours_ece_2026/cours_01/lib/login/login_page.dart
@@ -10,12 +10,73 @@ class LoginPage extends StatelessWidget {
         title: const Text(
           'Log in or sign up',
           style: TextStyle(
-            color: Color.fromARGB(255, 0, 0, 0), // Couleur noir
-            fontSize: 32, // Taille 
-            fontWeight: FontWeight.bold,//
+            color: Colors.black,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
+     
+      body: Column(
+        children: [
+          const EmailField(),
+          ContinueButton(onPressed: () {}),
+          const OrSeparator(),
+          const SocialLoginButton(
+            label: 'Apple',
+            iconPath: 'assets/apple_logo.svg',
+            onPressed: null,
+          ),
+        ],
+      ),
     );
+  }
+} 
+
+
+class EmailField extends StatelessWidget {
+  const EmailField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Champ adresse email');
+  }
+}
+
+class ContinueButton extends StatelessWidget {
+  final VoidCallback? onPressed; 
+  const ContinueButton({super.key, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Bouton Continuer');
+  }
+}
+
+class OrSeparator extends StatelessWidget {
+  const OrSeparator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Or');
+  }
+}
+
+class SocialLoginButton extends StatelessWidget {
+  
+  final String? label;
+  final String? iconPath;
+  final VoidCallback? onPressed;
+
+  const SocialLoginButton({
+    super.key, 
+    this.label, 
+    this.iconPath, 
+    this.onPressed
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Bouton Continuer avec $label');
   }
 }
